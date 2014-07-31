9325bc4d5793ab421df081f8d2fab073d795613b
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2014-07-31 12:39:38 +0200

    diode and transistor example fixe

--- examples/diode-dc-curve.py
+++ examples/diode-dc-curve.py
@@ -2,6 +2,7 @@
 
 import os
 
+import numpy as np
 from matplotlib import pylab
 
 ####################################################################################################
@@ -29,20 +30,50 @@ circuit.include(spice_library['1N4148'])
 circuit.include(spice_library['d1n5919brl'])
 
 circuit.V('input', 'in', circuit.gnd, '10V')
-circuit.R(1, 'in', 'out', kilo(1)) # not required for simulation
+circuit.R(1, 'in', 'out', 1) # not required for simulation
 # circuit.X('D1', '1N4148', 'out', circuit.gnd)
 circuit.X('DZ1', 'd1n5919brl', 'out', circuit.gnd)
 
 simulator = circuit.simulator(temperature=25, nominal_temperature=25)
-analysis = simulator.dc(Vinput=slice(-20, 20, .1))
+analysis = simulator.dc(Vinput=slice(-10, 2, .05)) # 10mV
 
-# pylab.plot(data['v(out)'], (data['v(in)'] - data['v(out)'])/1000)
-pylab.plot(analysis.out, -analysis.vinput) # .i
-pylab.axvline(x=-5.6, color='blue')
-pylab.legend(('Diode curve',), loc=(.1,.8))
-pylab.grid()
-pylab.xlabel('Voltage [V]')
-pylab.ylabel('Current [A]')
+# Fixme: i(vinput) -> analysis.vinput
+
+figure = pylab.figure()
+
+zener_part = analysis.out <= -5.4
+# compute derivate
+# fit linear part
+
+axe = pylab.subplot(121)
+axe.grid()
+# Fixme: scale
+axe.plot(analysis.out, -analysis.vinput*1000)
+axe.axvline(x=0, color='black')
+axe.axvline(x=-5.6, color='red')
+axe.axvline(x=1, color='red')
+axe.legend(('Diode curve',), loc=(.1,.8))
+axe.set_xlabel('Voltage [V]')
+axe.set_ylabel('Current [mA]')
+
+axe = pylab.subplot(122)
+axe.grid()
+# Fixme:
+# U = RI   R = U/I
+dynamic_resistance = np.diff(-analysis.out) / np.diff(analysis.vinput)
+# axe.plot(analysis.out[:-1], dynamic_resistance/1000)
+axe.semilogy(analysis.out[10:-1], dynamic_resistance[10:], basey=10)
+axe.axvline(x=0, color='black')
+axe.axvline(x=-5.6, color='red')
+axe.legend(('Dynamic Resistance',), loc=(.1,.8))
+axe.set_xlabel('Voltage [V]')
+axe.set_ylabel('Dynamic Resistance [Ohm]')
+
+# coefficients = np.polyfit(analysis.out[zener_part], dynamic_resistance[zener_part], deg=1)
+# x = np.array((min(analysis.out[zener_part]), max(analysis.out[zener_part])))
+# y = coefficients[0]*x + coefficients[1]
+# axe.semilogy(x, y, 'red')
+
 pylab.show()
 
 ####################################################################################################
--- examples/transistor.py
+++ examples/transistor.py
@@ -24,69 +24,53 @@ spice_library = SpiceLibrary(libraries_path)
 
 ####################################################################################################
 
-# circuit = Circuit('Transistor')
-
-# Vemitter = circuit.V('emitter', 'emitter', circuit.gnd, 1)
-# Vcollector = circuit.V('collector', circuit.gnd, 'collector', 10)
-# circuit.BJT(1, 'collector', circuit.gnd, 'emitter', 'generic')
-# circuit.model('generic', 'npn')
-
-# simulator = circuit.simulator(temperature=25, nominal_temperature=25)
-# analysis = simulator.dc(Vemitter=slice(-1, 2, .1))
-
-# # Fixme: Vemitter -> vemitter .i
-# pylab.plot(analysis.emitter, analysis.vemitter)
-# # pylab.axvline(x=-5.6, color='blue')
-# pylab.legend(('Diode curve',), loc=(.1,.8))
-# pylab.grid()
-# pylab.xlabel('Voltage [V]')
-# pylab.ylabel('Current [A]')
-# pylab.show()
-
-####################################################################################################
-
 circuit = Circuit('Transistor')
 
-Vbase = circuit.V('base', '1', circuit.gnd, 3)
-circuit.R('base', 1, 'base', kilo(100))
-Vcollector = circuit.V('collector', 'collector', circuit.gnd, 5)
+Vbase = circuit.V('base', '1', circuit.gnd, 1)
+circuit.R('base', 1, 'base', kilo(1))
+Vcollector = circuit.V('collector', '2', circuit.gnd, 0)
+circuit.R('collector', 2, 'collector', kilo(1))
 circuit.BJT(1, 'collector', 'base', circuit.gnd, 'generic')
 circuit.model('generic', 'npn')
+
+####################################################################################################
 
 figure = pylab.figure()
-axe = pylab.subplot(111)
+
+####################################################################################################
+
+simulator = circuit.simulator(temperature=25, nominal_temperature=25)
+analysis = simulator.dc(Vbase=slice(-1, 3, .01))
+
+axe = pylab.subplot(121)
+# Fixme: i(vinput) -> analysis.vinput
+axe.plot(analysis.base, -analysis.vbase*1000)
+axe.axvline(x=.65, color='red')
+axe.legend(('Base-Emitter Diode curve',), loc=(.1,.8))
 axe.grid()
-for base_voltage in np.arange(-1, 5, 1):
-    Vbase.dc_value = base_voltage
-
-    simulator = circuit.simulator(temperature=25, nominal_temperature=25)
-    analysis = simulator.dc(Vcollector=slice(-5, 5, .01))
-
-    # Fixme: lower case 
-    # pylab.plot(analysis.vbase, analysis.vcollector)
-    # pylab.plot(analysis.collector, -analysis.vbase*1e6)
-    axe.plot(analysis.collector, -analysis.vcollector)
+axe.set_xlabel('Voltage [V]')
+axe.set_ylabel('Current [mA]')
 
 ####################################################################################################
 
 circuit = Circuit('Transistor')
-# Ibase = circuit.I('base', 'base', circuit.gnd, micro(10))
-Ibase = circuit.I('base', '1', circuit.gnd, micro(10))
-circuit.R('base', 1, 'base', milli(1))
+Ibase = circuit.I('base', circuit.gnd, 'base', micro(10)) # take care to the orientation
 Vcollector = circuit.V('collector', 'collector', circuit.gnd, 5)
 circuit.BJT(1, 'collector', 'base', circuit.gnd, 'generic')
 circuit.model('generic', 'npn')
 
-# Fixme: ngspice doesn't support current sweep ???
+# Fixme: ngspice doesn't support multi-sweep ???
+#   it works in interactive mode
+# simulator = circuit.simulator(temperature=25, nominal_temperature=25)
 # analysis = simulator.dc(Vcollector=slice(0, 5, .1), Ibase=slice(micro(10), micro(100), micro(10)))
-# 0 v(i-sweep)    voltage
+# 0 v(i-sweep)    voltage # Vcollector in fact
 # 1 v(collector)  voltage
 # 2 v(base)       voltage
 # 3 i(vcollector) current
-# [  4.00000000e+00   5.00000000e+00   6.00000000e+00   4.00000000e+00 5.00000000e+00   6.00000000e+00]
-# [  4.00000000e+00   5.00000000e+00   6.00000000e+00   4.00000000e+00 5.00000000e+00   6.00000000e+00]
-# [ -4.99999800e+06  -4.99999750e+06  -4.99999700e+06  -9.99999800e+06 -9.99999750e+06  -9.99999700e+06]
-# [ -5.00000600e-06  -5.00000750e-06  -5.00000900e-06  -1.00000060e-05 -1.00000075e-05  -1.00000090e-05]
+# 0.00000000e+00,   1.00000000e-01,   2.00000000e-01, 3.00000000e-01,   4.00000000e-01,   5.00000000e-01, 6.00000000e-01,   7.00000000e-01,   8.00000000e-01, 9.00000000e-01
+# 0.00000000e+00,   1.00000000e-01,   2.00000000e-01, 3.00000000e-01,   4.00000000e-01,   5.00000000e-01, 6.00000000e-01,   7.00000000e-01,   8.00000000e-01, 9.00000000e-01
+# 6.50478604e-01,   7.40522920e-01,   7.68606463e-01, 7.69192913e-01,   7.69049191e-01,   7.69050844e-01, 7.69049584e-01,   7.69049559e-01,   7.69049559e-01, 7.69049559e-01
+# 9.90098946e-06,  -3.15540984e-04,  -9.59252614e-04, -9.99134834e-04,  -9.99982226e-04,  -1.00005097e-03, -1.00000095e-03,  -9.99999938e-04,  -9.99999927e-04, -9.99999937e-04
 
 # analysis = simulator.dc(Vcollector=slice(0, 10, .1))
 # 0 v(v-sweep)      voltage
@@ -97,22 +81,20 @@ circuit.model('generic', 'npn')
 # analysis = simulator.dc(Ibase=slice(micro(10), micro(100), micro(10)))
 # 0 v(i-sweep)      voltage
 # 1 v(collector)    voltage
-# 2 v(base) voltage
+ # 2 v(base) voltage
 # 3 i(vcollector)   current
 
-# Fixme: something is wrong Icollector(Vcollector) = cst
-figure = pylab.figure()
-axe = pylab.subplot(111)
+axe = pylab.subplot(122)
 axe.grid()
-for base_current in np.arange(1, 50, 10):
-    Ibase.dc_value = milli(base_current)
-
+# axe.legend(('Ic(Vce, Ib)',), loc=(.5,.5))
+axe.set_xlabel('Vce [V]')
+axe.set_ylabel('Ic [mA]')
+for base_current in np.arange(0, 100, 10):
+    Ibase.dc_value = micro(base_current)
     simulator = circuit.simulator(temperature=25, nominal_temperature=25)
-    analysis = simulator.dc(Vcollector=slice(-1, 5, .01))
-
+    analysis = simulator.dc(Vcollector=slice(0, 5, .01))
     # Fixme: lower case 
-    # pylab.plot(analysis.vbase, analysis.vcollector)
-    axe.plot(analysis.collector, -analysis.vcollector)
+    axe.plot(analysis.collector, -analysis.vcollector*1000)
 
 ####################################################################################################
 
