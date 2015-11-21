c142b281e6ad69775e57a7083cc37190794b66aa
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2015-11-21 18:34:25 +0100

    start script

--- start.sh
+++ start.sh
@@ -10,14 +10,9 @@
 # Build
 python setup.py build
 
-# Update TAGS file
-./tools/update-tags
-
-# Generate RST files
-##./tools/generate-rst
-# Generate HTML Documentation
-# cd sphinx/
-##./make-html --clean
+# Generate Documentation
+. tools/generate-rst-api.sh
+. tools/generate-rst-examples.sh
 
 ####################################################################################################
 #
