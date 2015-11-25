31133f5d31f2cb8e6925442cd82f15d53b6c94a1
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2015-11-25 21:29:23 +0100

    Completed netlist parsing

--- PySpice/Spice/Parser.py
+++ PySpice/Spice/Parser.py
@@ -28,6 +28,19 @@ It would be difficult to implement a full parser for Ngspice since the syntax is
 
 ####################################################################################################
 
+import logging
+
+####################################################################################################
+
+from .Netlist import ElementParameterMetaClass, NPinElement
+from .BasicElement import SubCircuitElement, BipolarJunctionTransistor
+
+####################################################################################################
+
+_module_logger = logging.getLogger(__name__)
+
+####################################################################################################
+
 class Token:
 
     """ This class implements a token, in fact a line in a Spice netlist. """
@@ -37,6 +50,55 @@ class Token:
     def __init__(self, line):
 
         self._line = line
+
+    ##############################################
+
+    def read_words(self, location_start, number_of_words):
+
+        line_str = str(self._line)
+        number_of_words_read = 0
+        words = []
+        while number_of_words_read < number_of_words: # and location_start < len(line_str)
+            location_stop = line_str.find(' ', location_start)
+            if location_stop == -1:
+                location_stop = None # read until end
+            word = line_str[location_start:location_stop].strip()
+            if word:
+                number_of_words_read += 1
+                words.append(word)
+            if location_stop is None: # we should stop
+                if number_of_words_read != number_of_words:
+                    raise NameError("Bad element line, looking for word:\n" +
+                                    line_str + '\n' +
+                                    ' '*location_start + '^')
+            else:
+                if location_start < location_stop:
+                    location_start = location_stop
+                else: # we have read a space
+                    location_start += 1
+        
+        return words, location_stop
+
+    ##############################################
+
+    def split_words(self, location_start, until=None):
+
+        line_str = str(self._line)
+        location_stop = None
+        if until is not None:
+            location = line_str.find(until, location_start)
+            if location != -1:
+                location_stop = location
+                location = line_str.rfind(' ', location_start, location_stop)
+                if location != -1:
+                    location_stop = location
+                else:
+                    raise NameError("Bad element line, missing key? " + line_str)
+        
+        line_str = line_str[location_start:location_stop]
+        words = [x for x in line_str.split(' ') if x]
+        
+        return words, location_stop
 
     ##############################################
 
@@ -146,17 +208,67 @@ class Element(Token):
 
     """ This class implements an element definition. """
 
+    _logger = _module_logger.getChild('Element')
+
     ##############################################
 
     def __init__(self, line):
 
         super().__init__(line)
 
-        parameters, dict_parameters = self.split_line('R')
-        self._element_type = str(line)[0]
-        self._name, self._nodes = parameters[0], parameters[1:]
-        self._parameters = parameters[2:]
-        self._dict_parameters = dict_parameters
+        line_str = str(line)
+        
+        self._prefix = line_str[0]
+        location_start = 1 # Fixme: start_location ?
+        location_stop = line_str.find(' ')
+        self._name = line_str[location_start:location_stop]
+        
+        self._parameters = None
+        self._dict_parameters = None
+        
+        classes = ElementParameterMetaClass.__classes__[self._prefix]
+        element_class = classes[0] # Fixme: all compatible ?
+        self._logger.debug(str(element_class) + '\n' + line_str)
+        if issubclass(element_class, NPinElement):
+            if issubclass(element_class, SubCircuitElement):
+                args, location_stop = self.split_words(location_stop, until='=')
+                self._nodes = args[:-1]
+                self._parameters = args[-1]
+            elif issubclass(element_class, BipolarJunctionTransistor):
+                self._nodes, location_stop = self.read_words(location_stop, 3)
+                # Fixme:
+            else:
+                raise NotImplementedError
+        else:
+            number_of_pins = element_class.number_of_pins()
+            if number_of_pins:
+                self._nodes, location_stop = self.read_words(location_stop, number_of_pins)
+            else:
+                self._nodes = []
+        
+        if location_stop is not None:
+            # args, location_stop = self.split_words(location_stop, until='=')
+            # number_of_positional_parameters = element_class.number_of_positional_parameters()
+            # if len(args) == number_of_positional_parameters:
+            #     self._parameters = args
+            # else:
+            #     template = "wrong number of positional parameters {} / {}"
+            #     raise NameError(template.format(len(args), number_of_positional_parameters))
+            number_of_positional_parameters = element_class.number_of_positional_parameters()
+            if number_of_positional_parameters:
+                self._parameters, location_stop = self.read_words(location_stop, number_of_positional_parameters)
+        if location_stop is not None:
+            has_optional_parameters = bool(element_class.optional_parameters)
+            if has_optional_parameters:
+                pass
+            else: # merge
+                self._parameters[-1] += line_str[location_stop:]
+
+        # parameters, dict_parameters = self.split_line('R') # tokenizer for () ...
+        # self._element_type = str(line)[0]
+        # self._name, self._nodes = parameters[0], parameters[1:] # Fixme: wrong #number of parameters
+        # self._parameters = parameters[2:]
+        # self._dict_parameters = dict_parameters
 
     ##############################################
 
@@ -169,7 +281,7 @@ class Element(Token):
 
     def __repr__(self):
 
-        return "Element {} {} {} {}".format(self._element_type, self._name, self._nodes, self._parameters)
+        return "Element {} {} {} {}".format(self._prefix, self._name, self._nodes, self._parameters)
 
 ####################################################################################################
 
@@ -239,13 +351,22 @@ class SpiceParser:
 
     """
 
+    _logger = _module_logger.getChild('SpiceParser')
+
     ##############################################
 
-    def __init__(self, path):
+    def __init__(self, path=None, source=None):
 
-        with open(str(path), 'r') as f:
-            raw_lines = f.readlines()
+        # Fixme: empty source
 
+        if path is not None:
+            with open(str(path), 'r') as f:
+                raw_lines = f.readlines()
+        elif source is not None:
+            raw_lines = source.split('\n') # Fixme: other os
+        else:
+            raise ValueError
+        
         lines = self._merge_lines(raw_lines)
         self._tokens = self._parse(lines)
         self._find_sections()
@@ -254,8 +375,12 @@ class SpiceParser:
 
     def _merge_lines(self, raw_lines):
 
-        """ Merge broken lines and return a new list of lines. """
+        """Merge broken lines and return a new list of lines.
+        
+        A line starting with "+" continues the preceding line.
+        """
 
+        # Fixme: better using lines[-1] ?
         lines = []
         current_line = ''
         current_line_index = None
@@ -282,7 +407,7 @@ class SpiceParser:
         for line in lines:
             # print repr(line)
             text = str(line)
-            lower_case_text = text.lower()
+            lower_case_text = text.lower() # !
             if text.startswith('*'):
                 tokens.append(Comment(line))
             elif lower_case_text.startswith('.'):
@@ -304,7 +429,11 @@ class SpiceParser:
                         tokens.append(model)
                 else:
                     # options param ...
-                    pass
+                    # .global .include .lib .param
+                    # .func .csparam .temp .if
+                    # { expr } are allowed in .model lines and in device lines.
+                    self._logger.warn(line)
+                    # pass
             else:
                 element = Element(line)
                 if sub_circuit:
