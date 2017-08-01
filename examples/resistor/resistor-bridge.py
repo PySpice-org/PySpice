64127e9d95b061f9f66d5d9e7b396926a45c24b4
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2017-08-01 20:43:27 +0200

    examples cleanup

--- examples/resistor/resistor-bridge.py
+++ examples/resistor/resistor-bridge.py
@@ -29,7 +29,3 @@ analysis = simulator.operating_point()
 for node in analysis.nodes.values():
     print('Node {}: {:4.1f} V'.format(str(node), float(node))) # Fixme: format value + unit
 #o#
-
-x = 1 + 2
-
-#!# X is @<@x@>@ \frac
