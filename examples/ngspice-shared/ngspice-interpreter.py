46f888a0e429e689a25076ee1dc97a8b4e1ab9be
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2017-09-07 22:19:17 +0200

    updated ngspice shared examples

--- examples/ngspice-shared/ngspice-interpreter.py
+++ examples/ngspice-shared/ngspice-interpreter.py
@@ -1,28 +1,12 @@
-#! /usr/bin/env python3
-
-####################################################################################################
-#
-# PySpice - A Spice Package for Python
-# Copyright (C) 2017 Fabrice Salvaire
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
 ####################################################################################################
 
-####################################################################################################
-
-import argparse
+#!#
+#!# =====================
+#!#  NgSpice Interpreter
+#!# =====================
+#!#
+#!# This example explains how to use the NgSpice binding.
+#!#
 
 ####################################################################################################
 
@@ -32,12 +16,6 @@ logger = Logging.setup_logging()
 ####################################################################################################
 
 from PySpice.Spice.NgSpice.Shared import NgSpiceShared
-
-####################################################################################################
-
-parser = argparse.ArgumentParser(description='Test Ngspice shared')
-
-args = parser.parse_args()
 
 ####################################################################################################
 
