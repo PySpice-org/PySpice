0434477722e9e4f584d293032a2de5c282c2e6d0
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2017-09-21 13:32:31 +0200

    fixed test_Netlist

--- unit-test/Spice/test_Netlist.py
+++ unit-test/Spice/test_Netlist.py
@@ -25,26 +25,26 @@ import unittest
 ####################################################################################################
 
 from PySpice.Spice.Netlist import *
-from PySpice.Unit.Units import *
+from PySpice.Unit import *
 
 ####################################################################################################
 
 class VoltageDivider(SubCircuitFactory):
     __name__ = 'VoltageDivider'
     __nodes__ = ('input', 'output_plus', 'output_minus')
-    def __init__(self, **kwargs):
-        super().__init__(title='Voltage Divider', **kwargs)
-        self.R(1, 'input', 'output_plus', kilo(9))
-        self.R(2, 'output_plus', 'output_minus', kilo(1))
+    def __init__(self):
+        super().__init__()
+        self.R(1, 'input', 'output_plus', 9@u_kΩ)
+        self.R(2, 'output_plus', 'output_minus', 1@u_kΩ)
 
 ####################################################################################################
 
 class VoltageDividerCircuit(Circuit):
-    def __init__(self, **kwargs):
-        super().__init__(title='Voltage Divider', **kwargs)
+    def __init__(self):
+        super().__init__(title='Voltage Divider')
         self.V('input', 'in', self.gnd, '10V')
-        self.R(1, 'in', 'out', kilo(9))
-        self.R(2, 'out', self.gnd, kilo(1))
+        self.R(1, 'in', 'out', 9@u_kΩ)
+        self.R(2, 'out', self.gnd, 1@u_kΩ)
 
 ####################################################################################################
 
@@ -74,8 +74,8 @@ class TestSubCircuit(TestNetlist):
 
         spice_declaration = """
 .subckt VoltageDivider input output_plus output_minus
-R1 input output_plus 9k
-R2 output_plus output_minus 1k
+R1 input output_plus 9kOhm
+R2 output_plus output_minus 1kOhm
 .ends VoltageDivider
 """
         self._test_spice_declaration(VoltageDivider(), spice_declaration)
@@ -91,15 +91,15 @@ class TestCircuit(TestNetlist):
         spice_declaration = """
 .title Voltage Divider
 Vinput in 0 10V
-R1 in out 9k
-R2 out 0 1k
-.end
+R1 in out 9kOhm
+R2 out 0 1kOhm
 """
+# .end
 
         circuit = Circuit('Voltage Divider')
         circuit.V('input', 'in', circuit.gnd, '10V')
-        circuit.R(1, 'in', 'out', kilo(9))
-        circuit.R(2, 'out', circuit.gnd, kilo(1))
+        circuit.R(1, 'in', 'out', 9@u_kΩ)
+        circuit.R(2, 'out', circuit.gnd, 1@u_kΩ)
         self._test_spice_declaration(circuit, spice_declaration)
 
         circuit = VoltageDividerCircuit()
@@ -107,10 +107,10 @@ R2 out 0 1k
 
         self._test_nodes(circuit, (0, 'in', 'out'))
 
-        self.assertEqual(circuit.R1.resistance, kilo(9))
-        self.assertEqual(circuit['R2'].resistance, kilo(1))
+        self.assertEqual(circuit.R1.resistance, 9@u_kΩ)
+        self.assertEqual(circuit['R2'].resistance, 1@u_kΩ)
 
-        circuit.R1.resistance = kilo(10)
+        circuit.R1.resistance = 10@u_kΩ
         self._test_spice_declaration(circuit, spice_declaration.replace('9k', '10k'))
 
         self.assertEqual(circuit.R1.plus.node, 'in')
