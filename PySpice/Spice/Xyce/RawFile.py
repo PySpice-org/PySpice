a70ae414d11768489488315ef11abccdfc2707e8
minecraft2048 <minecraft2048@gmail.com>
2018-06-10 14:39:30 +1000

    Fix odict errors

--- PySpice/Spice/Xyce/RawFile.py
+++ PySpice/Spice/Xyce/RawFile.py
@@ -157,8 +157,8 @@ class RawFile(RawFileAbc):
         """ Ngspice return lower case names. This method fixes the case of the variable names. """
 
         circuit = self.circuit
-        element_translation = {element.upper():element for element in circuit.element_names()}
-        node_translation = {node.upper():node for node in circuit.node_names()}
+        element_translation = {element.upper():element for element in circuit.element_names}
+        node_translation = {node.upper():node for node in circuit.node_names}
         for variable in self.variables.values():
             variable.fix_case(element_translation, node_translation)
 
