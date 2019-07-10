989969be89ffcd2ccf5892906f4a1187b494f44f
Jose M. Gomez <chemoki@gmail.com>
2019-07-10 10:18:21 +0200

    Update test_SpiceParser.py

--- unit-test/Unit/test_SpiceParser.py
+++ unit-test/Unit/test_SpiceParser.py
@@ -7,7 +7,10 @@ class TestSpiceParser(unittest.TestCase):
     def test_subcircuit(self):
         print(os.getcwd())
         circuit = Circuit('Diode Characteristic Curve')
-        circuit.include(os.path.join(os.getcwd(), 'ucc27211.lib'))
+        circuit.include(os.path.join(os.getcwd(), 'mosdriver.lib'))
+        circuit.X('test', 'mosdriver', '0', '1', '2', '3', '4', '5')
+        circuit.BehavioralSource('test', '1', '0', voltage_expression='if(0, 0, 1)', smoothbsrc=1)
+        print(circuit)
         self.assertEqual(True, False)
 
 
