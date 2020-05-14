1ab1971e427217532e1baf6ffccddcd08300213f
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2020-05-14 18:52:43 +0200

    add get_commits to git task

--- tasks/git.py
+++ tasks/git.py
@@ -20,11 +20,57 @@
 
 ####################################################################################################
 
+import datetime
+fromtimestamp = datetime.datetime.fromtimestamp
+
 from invoke import task
  # import sys
+
+try:
+    import pygit2 as git
+except:
+    git = None
 
 ####################################################################################################
 
 @task
 def uncommit(ctx):
     ctx.run('git reset --soft HEAD~')
+
+####################################################################################################
+
+@task
+def get_commits(ctx):
+
+    path = '.'
+    repository_path = git.discover_repository(path)
+    # print(repository_path)
+    repository = git.Repository(repository_path)
+
+    head = repository.head
+    head_commit = repository[head.target]
+
+    # GIT_SORT_TOPOLOGICAL. Sort the repository contents in topological order (parents before children);
+    # this sorting mode can be combined with time sorting.
+    sorting = git.GIT_SORT_TOPOLOGICAL # git.GIT_SORT_TIME
+    commits = [commit for commit in repository.walk(head_commit.id, sorting)]
+
+    template = '''
+====================================================================================================
+{message}
+
+  -------------------------------------------------
+  {name} <{email}> / {timestamp}
+  {id}
+'''
+
+    for commit in commits:
+        fields = {
+            'message': commit.message.strip(),
+            'id': commit.hex,
+            'timestamp': fromtimestamp(commit.commit_time).strftime('%Y-%m-%d %H:%M:%S'),
+            'name': commit.committer.name, # author
+            'email': commit.committer.email,
+        }
+        if 'salvaire' not in fields['name'].lower():
+            print(template.lstrip().format(**fields))
