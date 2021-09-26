88bfb52902ef184e6b61af715ccd5238c9991f17
Gulshan Singh <gsingh2011@gmail.com>
2021-09-26 14:41:36 -0700

    Bump Ngspice version to 35

--- PySpice/Spice/NgSpice/SimulationType.py
+++ PySpice/Spice/NgSpice/SimulationType.py
@@ -82,7 +82,7 @@ SIMULATION_TYPE[27] = (
     'charge',
 )
 
-LAST_VERSION = 34   # released on January 31st, 2021
+LAST_VERSION = 35   # released on August 8th, 2021
 
 for version in range(28, LAST_VERSION +1):
     SIMULATION_TYPE[version] = SIMULATION_TYPE[27]
