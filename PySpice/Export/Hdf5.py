7d30f0a3e0060907e0a12b736a0c763e1f547cc7
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2021-11-25 16:31:03 +0100

    cleanup unexpected copy-paste

--- PySpice/Export/Hdf5.py
+++ PySpice/Export/Hdf5.py
@@ -1,7 +1,7 @@
 ####################################################################################################
 #
 # PySpice - A Spice Package for Python
-# Copyright (C) 2014 Fabrice Salvaire
+# Copyright (C) 2021 Fabrice Salvaire
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
