cd7f725bf0144af2bef6751b716e0996260529fb
jmgc <chemoki@gmail.com>
2023-06-26 08:04:59 +0200

    Update RawFile.py
    Change to numpy frombuffer to avoid warning.

--- PySpice/Spice/RawFile.py
+++ PySpice/Spice/RawFile.py
@@ -296,7 +296,7 @@ class RawFileAbc:
         else:
             raise NotImplementedError
 
-        input_data = np.fromstring(raw_data, count=number_of_columns*self.number_of_points, dtype='f8')
+        input_data = np.frombuffer(raw_data, count=number_of_columns*self.number_of_points, dtype='f8')
         input_data = input_data.reshape((self.number_of_points, number_of_columns))
         input_data = input_data.transpose()
         # np.savetxt('raw.txt', input_data)
