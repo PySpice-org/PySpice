e7298638d2f23b86e0dbf9f0c444f9e725a908fe
“Alihossein <alih.sepahvand@gmail.com>
2025-04-04 15:49:35 -0600

    fix: uncomment libraries_path and spice_library initialization in simple_nand.py for proper library loading

--- examples/skywater/simple_nand.py
+++ examples/skywater/simple_nand.py
@@ -4,14 +4,14 @@ from PySpice.Unit import *
 
 ####################################################################################################
 
-# libraries_path = "/home/asepahvand/repos/skywater-pdk/libraries/sky130_fd_pr/latest/models/sky130.lib.spice"
-# spice_library = SpiceLibrary(libraries_path)
+libraries_path = "/home/asepahvand/repos/skywater-pdk/libraries/sky130_fd_pr/latest/models/sky130.lib.spice"
+spice_library = SpiceLibrary(libraries_path)
 
 ####################################################################################################
 
-#?# circuit_macros('buck-converter.m4')
-libraries_path = find_libraries()
-spice_library = SpiceLibrary(libraries_path)
+# #?# circuit_macros('buck-converter.m4')
+# libraries_path = find_libraries()
+# spice_library = SpiceLibrary(libraries_path)
 
 ####################################################################################################
 
