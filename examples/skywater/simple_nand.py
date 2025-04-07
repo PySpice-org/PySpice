86bc3384ef2879b33914baad3a28280c98b41f46
“Alihossein <alih.sepahvand@gmail.com>
2025-04-06 21:59:23 -0600

    fix: added recurseive include resolving in the code

--- examples/skywater/simple_nand.py
+++ examples/skywater/simple_nand.py
@@ -4,9 +4,9 @@ from PySpice.Unit import *
 
 ####################################################################################################
 
-libraries_path = "/home/asepahvand/repos/skywater-pdk/libraries/sky130_fd_pr/latest/models/sky130.lib.spice"
-# libraries_path = "/home/asepahvand/repos/spice_libraries/generic_format.lib"
-spice_library = SpiceLibrary(libraries_path)
+# libraries_path = "/home/asepahvand/repos/skywater-pdk/libraries/sky130_fd_pr/latest/models/sky130.lib_custom.spice"
+libraries_path = "/home/asepahvand/repos/spice_libraries/generic_format.lib"
+spice_library = SpiceLibrary(libraries_path, recurse=False)
 
 ####################################################################################################
 
@@ -19,6 +19,6 @@ spice_library = SpiceLibrary(libraries_path)
 #?# circuit_macros('buck-converter.m4')
 
 circuit = Circuit('Buck Converter')
-# circuit.include(spice_library['generic_comp'])
+circuit.include(spice_library['tt'])
 
 print(circuit)
