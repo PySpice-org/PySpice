b6b6d4d773e827057cedf751744f19d2f790c7f0
“Alihossein <alih.sepahvand@gmail.com>
2025-04-16 13:22:53 -0600

    feat: add example for ParallelResistor2 subcircuit with logging and validation

---
+++ examples/basic-usages/subcircuit2.py
@@ -0,0 +1,33 @@
+
+import PySpice.Logging.Logging as Logging
+logger = Logging.setup_logging()
+
+####################################################################################################
+
+from PySpice import Circuit, SubCircuit, SubCircuitFactory
+from PySpice.Spice.Parser.HighLevelParser import SpiceSource
+from PySpice.Spice.Parser.Translator import Builder
+from PySpice.Unit import *
+
+class ParallelResistor2(SubCircuit):
+    __nodes__ = ('n1', 'n2')
+    def __init__(self, name, R1=1@u_Ω, R2=2@u_Ω):
+        SubCircuit.__init__(self, name, *self.__nodes__)
+        self.R(1, 'n1', 'n2', R1)
+        self.R(2, 'n1', 'n2', R2)
+
+circuit = Circuit('Test')
+circuit.subcircuit(ParallelResistor2('pr1', R2=2@u_Ω))
+circuit.X('1', 'pr1', 1, circuit.gnd)
+circuit.subcircuit(ParallelResistor2('pr2', R2=3@u_Ω))
+circuit.X('2', 'pr2', 1, circuit.gnd)
+
+source = str(circuit)
+circuit_builder = Builder()
+parser = SpiceSource(source=source)
+bootstrap_circuit = circuit_builder.translate(spice_code=parser)
+bootstrap_source = str(bootstrap_circuit)
+
+print(bootstrap_source)
+
+assert bootstrap_source == source
