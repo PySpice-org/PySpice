11c488b34a622eb083029fafca9abec31ed9a938
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2026-09-03 22:56:12 +0200

    set last ngspice version to 47

--- PySpice/Spice/NgSpice/SimulationType.py
+++ PySpice/Spice/NgSpice/SimulationType.py
@@ -70,7 +70,7 @@ SIMULATION_TYPE[27] = (
     'charge',
 )
 
-LAST_VERSION = 42   # released on 2023-12-27
+LAST_VERSION = 47  # released on 2026-08-29
 
 for version in range(28, LAST_VERSION + 1):
     SIMULATION_TYPE[version] = SIMULATION_TYPE[27]
